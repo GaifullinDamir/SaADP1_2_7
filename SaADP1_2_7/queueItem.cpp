90c7c7634f6498c0635db19f57ce5660882c37a8
Gaifullin Damir <101250473+GaifullinDamir@users.noreply.github.com>
2022-03-23 23:48:53 +0300

    V0.1

--- SaADP1_2_7/queueItem.cpp
+++ SaADP1_2_7/queueItem.cpp
@@ -1,1 +1,65 @@
+#include <iostream>
 #include "queueItem.h"
+
+void init(QueueItem*& pFirst, QueueItem*& pLast)
+{
+	pFirst->pNext =  NULL;
+	pLast = pFirst;
+}
+
+bool isEmpty(QueueItem* pFirst, QueueItem* pLast)
+{
+	if (pFirst == pLast) { return true; }
+	else { return false; }
+}
+
+void addItem(QueueItem*& pFirst, QueueItem*& pLast, int data)
+{
+	if (isEmpty(pFirst, pLast))
+	{
+		pFirst->pNext = pLast;
+	}
+	QueueItem* temporary = new QueueItem;
+	temporary->data = data;
+	temporary->pNext = NULL;
+	pLast->pNext = temporary;
+	pLast = temporary;
+}
+
+void deleteItem(QueueItem*& pFirst, QueueItem*& pLast)
+{
+	QueueItem* temporary = new QueueItem;
+	if (!isEmpty(pFirst, pLast))
+	{
+		temporary = pFirst->pNext;
+		pFirst->pNext = temporary->pNext;
+		delete temporary;
+	}
+	else
+	{
+		std::cout << std::endl;
+		std::cout << "   The queue is empty. Nothing to delete." << std::endl;
+		std::cout << std::endl;
+	}
+}
+
+void printQueue(QueueItem*& pFirst, QueueItem*& pLast)
+{
+	if (isEmpty(pFirst, pLast))
+	{
+		std::cout << std::endl;
+		std::cout << "   The queue is empty. Nothing to print." << std::endl;
+		std::cout << std::endl;
+	}
+	else
+	{
+		std::cout << std::endl;
+		QueueItem* current = pFirst->pNext;
+		while (current != NULL)
+		{
+			std::cout << "   " << current->data << std::endl;
+			current = current->pNext;
+		}
+		std::cout << std::endl;
+	}
+}
--- SaADP1_2_7/queueItem.h
+++ SaADP1_2_7/queueItem.h
@@ -1,5 +1,16 @@
 #pragma once
-struct queueItem
+struct QueueItem
 {
+	int data;
+	QueueItem* pNext;
 };
 
+void init(QueueItem*& pFirst, QueueItem*& pLast);
+
+bool isEmpty(QueueItem* pFirst, QueueItem* pLast);
+
+void addItem(QueueItem*& pFirst, QueueItem*& pLast, int data);
+
+void deleteItem(QueueItem*& pFirst, QueueItem*& pLast);
+
+void printQueue(QueueItem*& pFirst, QueueItem*& pLast);
